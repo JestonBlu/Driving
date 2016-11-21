27ff70907341449bb6938f653ee2620b0cb6e02e
jestonblu <jestonblu@gmail.com>
2016-11-21 09:11:04 -0600

    added weight diagnostics

--- R-Scripts/08_weights.R
+++ R-Scripts/08_weights.R
@@ -1,46 +1,50 @@
-load("R-Models/mdl_13_nnet.rda")
-load("R-Data/data-mdl-13.rda")
+rm(list = ls())
 
+library(nnet)
 
-#import the function from Github
-library(devtools)
-source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
+load("R-Data/data-mdl-08.rda")
 
-#plot each model
-plot.nnet(mdl.13, wts.only = TRUE)
+dummies = model.matrix(~ mdl.08.train$Subject - 1)
+colnames(dummies) = sort(as.character(unique(mdl.08.train$Subject)))
+mdl.13.train = cbind(dummies, mdl.08.train[, c(2:4,6:13)])
 
+fun = function(x) 1 / (1 + exp(-x))
 
-## Old Male
--.98+2.25-1.57; 1 / (1 + exp(.3))
+results = data.frame()
 
-## Young Male
--.98-1.57; 1 / (1 + exp(2.55))
-
-.425*## Old Female 
--.98+2.25; 1 / (1 + exp(-1.27))
-
-## Young Female
--.98; 1/(1 + exp(.98))
-
-
-1/(1 + exp(-.834))
-
-
-source_url('https://gist.githubusercontent.com/fawda123/6206737/raw/d6f365c283a8cae23fb20892dc223bc5764d50c7/gar_fun.r')
-
-#create a pretty color vector for the bar plot
-cols<-colorRampPalette(c('lightgreen','lightblue'))(8)
-
-#use the function on the model created above
-par(mar=c(3,4,1,1),family='serif')
-
-gar.fun('Texting', mdl.13) + coord_flip()
+for (i in 1:100) {
+  
+  fit = nnet(Texting ~  Age_Old * Gender_Male + .,
+             data = mdl.13.train,
+             range = .1,
+             size = 1, 
+             maxit = 100, 
+             decay = .1)
+  
+  OM = fit$wts[1] + fit$wts[2] + fit$wts[3] + fit$wts[71]
+  YM = fit$wts[1] + fit$wts[3]
+  OF = fit$wts[1] + fit$wts[2]
+  YF = fit$wts[1]
+  B1 = fit$wts[1]
+  B2 = fit$wts[72]
+  O1 = fit$wts[73]
+  
+  results = rbind(results, data.frame(Trial = i, OM, YM, OF, YF, B1, B2, O1))
+  
+}
 
 
 
-#import the function from Github
-source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
+########################################################
+########################################################
 
-#plot each model
-plot.nnet(mdl.13)
+
+
+
+
+
+
+
+
+
 
